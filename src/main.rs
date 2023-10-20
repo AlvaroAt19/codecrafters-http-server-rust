b90a08bc0aca3055638249e729009cb8c25be1c6
AlvaroAt19 <alvaroalves11@hotmail.com>
2023-10-20 15:15:30 -0300

    Handling 404

--- src/main.rs
+++ src/main.rs
@@ -24,13 +24,22 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
-    let mut buffer = [0; 128];
+    let mut buffer: [u8; 128] = [0; 128];
     
     stream.read(&mut buffer)?;
     
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let parsed_vec: Vec<&str> = std::str::from_utf8(&buffer).unwrap()
+                                    .split("\r\n").collect();
 
-    stream.write(response.as_bytes())?;
+    let route: &str = parsed_vec[0].split_whitespace().collect::<Vec<&str>>()[1];
 
+    let ok_response: &str = "HTTP/1.1 200 OK\r\n\r\n";
+    let error_response: &str = "HTTP/1.1 404 Not Found\r\n\r\n";
+    
+    match route{
+        "/" => stream.write(ok_response.as_bytes())?, 
+        _ => stream.write(error_response.as_bytes())?
+    };
+    
     Ok(())
 }
